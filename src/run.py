c08b62f9694e218534b66a33027239a7769fd75b
Zhivaev Artem <zhivaev993@gmail.com>
2021-07-23 22:13:12 +0300

    long pooling

--- src/run.py
+++ src/run.py
@@ -1,6 +1,8 @@
 from os import system
 from shikilogsparser import retrieve_new_logs_by_usernames
 from os import path
+import time
+from datetime import datetime
 
 
 if __name__ == "__main__":
@@ -13,5 +15,11 @@ if __name__ == "__main__":
             line = line.strip('\r\n \t')
             if (line):
                 usernames.append(line)
-    grouped_logs = retrieve_new_logs_by_usernames(usernames)
-    print(grouped_logs)
+    while True:
+        grouped_logs = retrieve_new_logs_by_usernames(usernames)
+        for username, user_logs in grouped_logs.items():
+            if user_logs:
+                print(datetime.now())
+                print(grouped_logs)
+                break
+        time.sleep(5 * 60)
