aa1904c88dcd6887567520886c4480480e28bd8b
Zhivaev Artem <zhivaev993@gmail.com>
2021-07-30 10:21:26 +0300

    pretty imports

--- src/shiki_bot/locallib/__init__.py
+++ src/shiki_bot/locallib/__init__.py
@@ -1,4 +1,4 @@
+from .bot_worker import BotWorkerTask
 from .config import Config
 from .datbase_adapter import DatabaseAdapter
 from .shiki_client import ShikiClient
-from .bot_worker import BotWorkerTask
--- src/shiki_bot/locallib/config.py
+++ src/shiki_bot/locallib/config.py
@@ -1,5 +1,4 @@
 import json
-from os import name
 
 import discord
 
