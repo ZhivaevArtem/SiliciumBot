35a37d8b3e826d0ed293c2cb6b80600842e070c9
Zhivaev Artem <zhivaev993@gmail.com>
2021-08-20 21:06:17 +0300

    Removed extra package

--- src/silicium_bot/locallib/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import Config
-from .datbase_adapter import DatabaseAdapter
-from .loop_requests import LoopRequestsTask
-from .shiki_client import ShikiClient
-from .timeout_task import invoke_timeout
