b2002240aba7774cf8ec21f82dad84b58617f0a8
Artem Zhivaev <zhivaev993@gmail.com>
2022-02-26 17:31:51 +0300

    issue-17: Line endings lf for all files

--- src/main/java/com/zhivaevartem/siliciumbot/App.java
+++ src/main/java/com/zhivaevartem/siliciumbot/App.java
@@ -1,12 +1,6 @@
 package com.zhivaevartem.siliciumbot;
 
-import com.zhivaevartem.siliciumbot.listeners.SampleEventListener;
 import com.zhivaevartem.siliciumbot.utils.ConfigUtils;
-import javax.security.auth.login.LoginException;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
 
 /**
  * Startup class.
@@ -19,17 +13,6 @@ public class App {
    */
   public static void main(String[] args) {
     String token = ConfigUtils.getProp("token");
-    JDABuilder builder = JDABuilder.createDefault(token);
-    JDA jda = null;
-    try {
-      jda = builder.build();
-    } catch (LoginException e) {
-      e.printStackTrace();
-      return;
-    }
-    jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
-    jda.getPresence().setActivity(Activity.listening("Музыка мясокомбината"));
-
-    jda.addEventListener(new SampleEventListener());
+    String connectionString = ConfigUtils.getProp("mongodb.connection_string");
   }
 }
