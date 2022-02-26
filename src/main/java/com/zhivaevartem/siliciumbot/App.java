d69ecfc26da4f00a3df1413469bdc4c11bf11d18
Artem Zhivaev <artem.zhivaev@netcracker.com>
2022-02-27 00:51:44 +0300

    issue-17: Added dependencies

--- src/main/java/com/zhivaevartem/siliciumbot/App.java
+++ src/main/java/com/zhivaevartem/siliciumbot/App.java
@@ -1,6 +1,10 @@
 package com.zhivaevartem.siliciumbot;
 
+import com.zhivaevartem.siliciumbot.persistence.entities.BotConfigLocal;
 import com.zhivaevartem.siliciumbot.utils.ConfigUtils;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 /**
  * Startup class.
@@ -13,6 +17,18 @@ public class App {
    */
   public static void main(String[] args) {
     String token = ConfigUtils.getProp("token");
-    String connectionString = ConfigUtils.getProp("mongodb.connection_string");
+    System.out.println(token);
+
+    EntityManagerFactory emFac = Persistence.createEntityManagerFactory("siliciumbot");
+    EntityManager em = emFac.createEntityManager();
+
+    BotConfigLocal cfg = new BotConfigLocal();
+    cfg.setGuildId("456894735684567867985746");
+    cfg.setPrefix("!");
+    em.getTransaction().begin();
+    em.persist(cfg);
+    em.getTransaction().commit();
+    em.close();
+    emFac.close();
   }
 }
