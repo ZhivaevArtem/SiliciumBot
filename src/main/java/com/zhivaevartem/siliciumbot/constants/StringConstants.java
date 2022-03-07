b1a60b7489cf776358d18af0c327ee7e78221666
Zhivaev Artem <zhivaev993@gmail.com>
2022-03-07 16:29:32 +0300

    issue-17: Added javadoc

--- src/main/java/com/zhivaevartem/siliciumbot/constants/StringConstants.java
+++ src/main/java/com/zhivaevartem/siliciumbot/constants/StringConstants.java
@@ -1,5 +1,8 @@
 package com.zhivaevartem.siliciumbot.constants;
 
+/**
+ * All string constants should be placed here.
+ */
 public class StringConstants {
   public static final String DEFAULT_BOT_COMMAND_PREFIX = "D";
 }
